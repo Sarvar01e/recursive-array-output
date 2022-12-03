97a35444c5e5f9d5025ae5350835aac24925a219
Sarvar01e <sernazarov185@gmail.com>
2022-12-03 20:43:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace recursive_array_output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            arrayoutput(array);
+     
+        }
+
+        static void arrayoutput (int[] inputarray, int index =0, int sum = 0) 
+        {
+            Console.Write(inputarray[index]+"\t");
+            sum = sum + inputarray[index];
+            index++;
+            if (index == inputarray.Length)
+            {
+                Console.WriteLine("\n\nSum is\t" + sum);
+                return;
+            }
+            arrayoutput(inputarray,index,sum);
+        }
+        static void find_the_sum_of_the_digits(int inputvalue) 
+        {
+            string valueout = Convert.ToString(inputvalue);
+
+
+        }
+    }
+}
