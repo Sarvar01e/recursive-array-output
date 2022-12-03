44a3e4ad14ac5d759e3702b6ff8600bb8a7dbb1e
Sarvar01e <sernazarov185@gmail.com>
2022-12-03 22:01:33 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@
         static void Main(string[] args)
         {
             int[] array = { 1, 2, 3, 4, 5 };
-            arrayoutput(array);
+           // arrayoutput(array);
+            int a = 515;
+            //sumreturner(ref a);
+            string value = Console.ReadLine();
+            summer(ref value);
      
         }
 
@@ -21,11 +25,39 @@
             }
             arrayoutput(inputarray,index,sum);
         }
-        static void find_the_sum_of_the_digits(int inputvalue) 
+        static void sumreturner(ref int inputvalue) 
         {
             string valueout = Convert.ToString(inputvalue);
+            int sum = 0;
+            for (int i = 0; i < valueout.Length; i++)
+            {
+                sum = sum+inputvalue%10;
+                inputvalue = inputvalue / 10;
+                if (i == valueout.Length - 1)
+                {
+                sum = sum + inputvalue;
+                }
+            }
+            inputvalue = sum;
+            Console.WriteLine(sum);
+        }
+        static void summer(ref string value1, int end =0, int sum=0) 
+        {
+            int inputvalue = Convert.ToInt32(value1);
+            sum = sum + inputvalue % 10;
 
-
+            inputvalue = inputvalue / 10;
+            string changed;
+            changed = Convert.ToString(inputvalue);
+            if (end > value1.Length)
+            {
+                sum = sum + inputvalue % 10;
+                Console.WriteLine("Sum of numbers is\t" + sum);
+                return;
+            }
+            end++;
+            summer(ref changed, end, sum);
+            
         }
     }
 }
